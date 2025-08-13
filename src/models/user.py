ea4ac959a3651c3df51ebbed4193e959a55daff0
ramonamo <81926562+ramonamo@users.noreply.github.com>
2025-08-14 01:22:58 +0300

    Delete src directory

--- src/models/user.py
+++
@@ -1,3 +0,0 @@
-# Not: SQLAlchemy kaldırıldı
-# Tüm veriler dosya sisteminde saklanıyor
-# Kullanıcı işlemleri şu an için devre dışı
--- src/routes/user.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-user_bp = Blueprint('user', __name__)
-
-# Not: Şu an için kullanıcı işlemleri devre dışı
-# Tüm işlemler anonim olarak yapılıyor ve veriler dosyalarda saklanıyor
